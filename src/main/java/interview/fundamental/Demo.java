8c95c24625c46d34c3f555bbcf2624650747ae5b
Junjia Liang <89063119@qq.com>
2018-10-10 22:45:54 +0800

    新增两道面试题

---
+++ src/main/java/interview/fundamental/Demo.java
@@ -0,0 +1,29 @@
+package interview.fundamental;
+
+/**
+ * 以下代码执行的结果显示是多少（）？
+ * Created by liangjunjia on 2018/10/10.
+ */
+public class Demo {
+    public static void main(String[] args) {
+        System.out.print(getNumber(0));
+        System.out.print(getNumber(1));
+        System.out.print(getNumber(2));
+        System.out.print(getNumber(4));
+    }
+
+    private static int getNumber(int num) {
+        try {
+            return 2 / num;
+        } catch (Exception exception) {
+            return 0;
+        } finally {
+            if (num == 0) {
+                return -1;
+            }
+            if (num == 1) {
+                return 1;
+            }
+        }
+    }
+}
---
+++ src/main/java/interview/fundamental/Foo.java
@@ -0,0 +1,19 @@
+package interview.fundamental;
+
+/**
+ * 以下代码结果是什么？
+ * Created by liangjunjia on 2018/10/10.
+ */
+public class Foo {
+    public static void main(String sgf[]) {
+        StringBuffer a = new StringBuffer("A");
+        StringBuffer b = new StringBuffer("B");
+        operate(a, b);
+        System.out.println(a + "." + b);
+    }
+
+    private static void operate(StringBuffer x, StringBuffer y) {
+        x.append(y);
+        y = x;
+    }
+}
