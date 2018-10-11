4527bef0e12a5a8546ba1b27bb9799022df5cedf
Junjia Liang <89063119@qq.com>
2018-10-11 22:17:18 +0800

    添加一道面试题

---
+++ src/main/java/interview/fundamental/Test2.java
@@ -0,0 +1,21 @@
+package interview.fundamental;
+
+/**
+ * 下列代码执行结果为（）
+ * Created by liangjunjia on 2018/10/11.
+ */
+public class Test2 {
+    public static void main(String args[]) throws InterruptedException {
+        Thread t = new Thread(() -> {
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            System.out.print("2");
+        });
+        t.start();
+        t.join();
+        System.out.print("1");
+    }
+}
