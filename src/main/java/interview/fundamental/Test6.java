7f581f71f14d6bc3deabf3a1b3d3662c4dcd5268
Junjia Liang <89063119@qq.com>
2018-10-16 21:15:16 +0800

    新增1道面试题

---
+++ src/main/java/interview/fundamental/Test6.java
@@ -0,0 +1,21 @@
+package interview.fundamental;
+
+/**
+ * 以下代码执行后输出结果为（ ）
+ * Created by liangjunjia on 2018/10/16.
+ */
+public class Test6 {
+    public static Test6 t1 = new Test6();
+
+    {
+        System.out.println("blockA");
+    }
+
+    static {
+        System.out.println("blockB");
+    }
+
+    public static void main(String[] args) {
+        Test6 t2 = new Test6();
+    }
+}
