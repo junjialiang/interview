85529e286058da5bf467adf05f2ccf379127fd6e
Junjia Liang <89063119@qq.com>
2018-10-21 14:12:16 +0800

    新增2道面试题

---
+++ src/main/java/interview/fundamental/Person.java
@@ -0,0 +1,13 @@
+package interview.fundamental;
+
+/**
+ * 关于如下程序的描述哪个是正确的？（ ）
+ * Created by liangjunjia on 2018/10/21.
+ */
+public class Person {
+    private static int arr[] = new int[5];
+
+    public static void main(String a[]) {
+        System.out.println(arr[0]);
+    }
+}
---
+++ src/main/java/interview/fundamental/Test11.java
@@ -0,0 +1,17 @@
+package interview.fundamental;
+
+/**
+ * 以下代码执行的结果显示是多少（）？
+ * Created by liangjunjia on 2018/10/21.
+ */
+public class Test11 {
+    public static void main(String args[]) {
+        int count = 0;
+        int num = 0;
+        for (int i = 0; i <= 100; i++) {
+            num = num + i;
+            count = count++;
+        }
+        System.out.println("num * count = " + (num * count));
+    }
+}
