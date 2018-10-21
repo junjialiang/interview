3a1ec73fb7179e1e510e53a80a44885152d22a32
Junjia Liang <89063119@qq.com>
2018-10-21 14:26:28 +0800

    代码优化

--- src/main/java/interview/fundamental/MyApplication.java
+++ src/main/java/interview/fundamental/MyApplication.java
@@ -1,7 +1,7 @@
 package interview.fundamental;
 
 /**
- * 阅读如下代码。 请问，对语句行 test.hello(). 描述正确的有（）
+ * 阅读如下代码。 请问，对语句行 test8.hello(). 描述正确的有（）
  * Created by liangjunjia on 2018/10/18.
  */
 class Test8 {
@@ -12,7 +12,7 @@ class Test8 {
 
 public class MyApplication {
     public static void main(String[] args) {
-        Test test = null;
+        Test8 test = null;
         Test8.hello();
     }
 }
