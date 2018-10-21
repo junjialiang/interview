3e958e75b7eb9289218d4b9a738b356feb1d27b7
Junjia Liang <89063119@qq.com>
2018-10-21 11:28:26 +0800

    新增1道面试题

---
+++ src/main/java/interview/fundamental/HasStatic.java
@@ -0,0 +1,22 @@
+package interview.fundamental;
+
+/**
+ * 关于以下程序代码的说明正确的
+ * Created by liangjunjia on 2018/10/20.
+ */
+public class HasStatic {
+
+    private static int x = 100;
+
+    public static void main(String args[]) {
+        HasStatic hs1 = new HasStatic();
+        hs1.x++;
+        HasStatic hs2 = new HasStatic();
+        hs2.x++;
+        hs1 = new HasStatic();
+        hs1.x++;
+        HasStatic.x--;
+        System.out.println("x=" + x);
+    }
+}
+
