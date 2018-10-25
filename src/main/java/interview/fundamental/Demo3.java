450acd8cd2359b34d2220c28ed8b363fac9fdd5f
Junjia Liang <89063119@qq.com>
2018-10-25 21:54:44 +0800

    新增一道面试题

---
+++ src/main/java/interview/fundamental/Demo3.java
@@ -0,0 +1,26 @@
+package interview.fundamental;
+
+/**
+ * 给定以下JAVA代码，这段代码运行后输出的结果是（）
+ * Created by liangjunjia on 2018/10/25.
+ */
+public class Demo3 {
+    private static int aMethod(int i) throws Exception {
+        try {
+            return i / 10;
+        } catch (Exception ex) {
+            throw new Exception("exception in a aMethod");
+        } finally {
+            System.out.printf("finally");
+        }
+    }
+
+    public static void main(String[] args) {
+        try {
+            aMethod(0);
+        } catch (Exception ex) {
+            System.out.printf("exception in main");
+        }
+        System.out.printf("finished");
+    }
+}
