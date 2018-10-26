ff752879e022abcdd880e6f7edc2e3b247cbe80d
Junjia Liang <89063119@qq.com>
2018-10-26 21:28:08 +0800

    新增一道面试题

---
+++ src/main/java/interview/fundamental/Demo4.java
@@ -0,0 +1,13 @@
+package interview.fundamental;
+
+/**
+ * 顺序执行下列程序语句后，则b的值是（）
+ * Created by liangjunjia on 2018/10/26.
+ */
+public class Demo4 {
+    public static void main(String[] args) {
+        String a = "Hello";
+        String b = a.substring(0, 2);
+        System.out.println(b);
+    }
+}
