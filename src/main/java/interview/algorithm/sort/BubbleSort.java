3856964652cea8fb2d40ae942d9d07896078a5c2
Junjia Liang <89063119@qq.com>
2019-04-19 10:28:39 +0800

    优化冒泡排序

--- src/main/java/interview/algorithm/sort/BubbleSort.java
+++ src/main/java/interview/algorithm/sort/BubbleSort.java
@@ -6,16 +6,13 @@ package interview.algorithm.sort;
  * @Description: 如何进行冒泡排序
  */
 public class BubbleSort {
-    public static void bubbleSort(int array[]) {
-        int i, j;
-        int len = array.length;
-        int tmp;
-        for (i = 0; i < len - 1; ++i) {
-            for (j = len - 1; j > i; --j) {
-                if (array[j] < array[j - 1]) {
-                    tmp = array[j];
-                    array[j] = array[j - 1];
-                    array[j - 1] = tmp;
+    public static void bubbleSort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {//-1为了防止溢出
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
                 }
             }
         }
@@ -25,7 +22,7 @@ public class BubbleSort {
         int a[] = {5, 4, 9, 8, 7, 6, 0, 1, 3, 2};
         bubbleSort(a);
         for (int i = 0; i < a.length; i++) {
-            System.out.println(a[i] + " ");
+            System.out.print(a[i] + " ");
         }
     }
 }
